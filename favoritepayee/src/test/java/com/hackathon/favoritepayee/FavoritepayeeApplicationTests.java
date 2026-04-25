d410fc225e67892bf9b53b2ca4555ee0d1254174
Nagendharreddy Kondapu <nagendharreddy22@gmail.com>
2026-04-25 12:24:13 +0530

    📦 NEW: Initial project set up

---
+++ favoritepayee/src/test/java/com/hackathon/favoritepayee/FavoritepayeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.favoritepayee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavoritepayeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
